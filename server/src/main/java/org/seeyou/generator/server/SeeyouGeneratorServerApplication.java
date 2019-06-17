8ab1e34fc4d6f2638798c65d0952105c6e3f2e1c
hongxiyou <hongxiyou@yeah.net>
2019-06-17 10:52:12 +0800

    初始化项目

---
+++ server/src/main/java/org/seeyou/generator/server/SeeyouGeneratorServerApplication.java
@@ -0,0 +1,13 @@
+package org.seeyou.generator.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeyouGeneratorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeyouGeneratorServerApplication.class, args);
+	}
+
+}
